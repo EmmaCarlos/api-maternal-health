82982063a104e0522dfd2ca179b80b12016f4134
EmmaCarlos <carlitos.benitez@yahoo.com>
2022-09-29 20:08:14 -0300

    subiendo database heroku

--- API/src/database/config/config.js
+++ API/src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports={
     "development": {
-      "username": "root",
-      "password": null,
-      "database": "api",
-      "host": "127.0.0.1",
+      "username": "b4b0a8cd707379",
+      "password": "d71e4260",
+      "database": "heroku_48ad915c770713c",
+      "host": "us-cdbr-east-06.cleardb.net",
       "dialect": "mysql"
     },
     "test": {
