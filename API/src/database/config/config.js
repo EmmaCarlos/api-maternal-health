7c1c781d0db32e3a264743624b80ab010b0eb615
Carlos Benitez <84422399+EmmaCarlos@users.noreply.github.com>
2022-09-30 15:41:57 -0300

    Update config.js

--- API/src/database/config/config.js
+++ API/src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports={
       "dialect": "mysql"
     },
     "test": {
-      "username": "root",
-      "password": null,
-      "database": "api",
-      "host": "127.0.0.1",
+      "username": "b77011e2530bee",
+      "password": "a7e6a25e",
+      "database": "heroku_80be73df4011766",
+      "host": "us-cdbr-east-06.cleardb.net",
       "dialect": "mysql"
     },
     "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "127.0.0.1",
+      "username": "b77011e2530bee",
+      "password": "a7e6a25e",
+      "database": "heroku_80be73df4011766",
+      "host": "us-cdbr-east-06.cleardb.net",
       "dialect": "mysql"
     }
 }
