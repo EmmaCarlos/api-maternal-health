20909f6289f2d88654bbfda32844d84da53a5115
EmmaCarlos <carlitos.benitez@yahoo.com>
2022-10-01 02:53:42 -0300

    final2

--- API/src/database/config/config.js
+++ API/src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports={
       "dialect": "mysql"
     },
     "test": {
-      "username": "b77011e2530bee",
-      "password": "a7e6a25e",
-      "database": "heroku_80be73df4011766",
-      "host": "us-cdbr-east-06.cleardb.net",
+      "username": "bzxo63ofhz7anxld",
+      "password": "tx486wqvh9smlpmm",
+      "database": "b52oqj7reqeyq24g",
+      "host": "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       "dialect": "mysql"
     },
     "production": {
-      "username": "b77011e2530bee",
-      "password": "a7e6a25e",
-      "database": "heroku_80be73df4011766",
-      "host": "us-cdbr-east-06.cleardb.net",
+      "username": "bzxo63ofhz7anxld",
+      "password": "tx486wqvh9smlpmm",
+      "database": "b52oqj7reqeyq24g",
+      "host": "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       "dialect": "mysql"
     }
 }
