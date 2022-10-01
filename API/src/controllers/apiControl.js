cefb69e333bca33a187e3ddecd6be2d28d9a1a26
EmmaCarlos <carlitos.benitez@yahoo.com>
2022-09-30 23:36:31 -0300

    adad

--- API/src/controllers/apiControl.js
+++ API/src/controllers/apiControl.js
@@ -1,22 +1,9 @@
 const {maternal_health} = require('../database/models');
 
 
-const controller = {
-    maternal_health:async (req,res)=>{
-        await maternal_health.findAll({
-                /* order:[
-                    ['Age','DESC']
-                ], */
-                attributes:['Age', 'SystolicBP','DiastolicBP','BS','BodyTemp','HeartRate','RiskLevel']
-                
-            }).then(/* users */maternal_health=>{
-                let resul={
-                    dataset:{
-                    cantidad:maternal_health.length},
-                    data:maternal_health
-                }
-                res.send(resul);
-            })
-        }
-        }
+const controller = {maternal_health: async (req,res)=>{await maternal_health.findAll({attributes:['Age', 'SystolicBP','DiastolicBP','BS','BodyTemp','HeartRate','RiskLevel']}).then(/* users */maternal_health=>{
+let resul={
+dataset:{cantidad:maternal_health.length},data:maternal_health}
+res.send(resul);})}}
+
 module.exports= controller
