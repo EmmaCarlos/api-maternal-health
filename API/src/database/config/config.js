d348f9ffcfcb619d096a55dc9f74e3517d2c370e
EmmaCarlos <carlitos.benitez@yahoo.com>
2022-10-01 02:50:41 -0300

    final

--- API/src/database/config/config.js
+++ API/src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports={
     "development": {
-      "username": "b77011e2530bee",
-      "password": "a7e6a25e",
-      "database": "heroku_80be73df4011766",
-      "host": "us-cdbr-east-06.cleardb.net",
+      "username": "bzxo63ofhz7anxld",
+      "password": "tx486wqvh9smlpmm",
+      "database": "b52oqj7reqeyq24g",
+      "host": "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       "dialect": "mysql"
     },
     "test": {
